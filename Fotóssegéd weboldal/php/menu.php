a111a67c587cb6c7b5c880dedfe1e475b603ada9
Márk Marosi <marosim99@gmail.com>
2019-03-17 01:01:34 +0100

    Fórum megvalósításának elkezdése

--- Fotóssegéd weboldal/php/menu.php
+++ Fotóssegéd weboldal/php/menu.php
@@ -19,7 +19,7 @@
       <a class="nav-link" href="#"> Feltöltések </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#"> Fórum </a>
+      <a class="nav-link" href="forum.php"> Fórum </a>
     </li>
   </ul>
   <ul class="navbar-nav ml-auto">
