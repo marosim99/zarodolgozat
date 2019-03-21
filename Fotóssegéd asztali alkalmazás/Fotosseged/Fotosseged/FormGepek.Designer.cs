97bb9d35c548c03547f77058a6e4fd5d1e557bb2
Márk Marosi <marosim99@gmail.com>
2019-03-21 13:31:48 +0100

    Form fejlesztése vezérlőkkel

--- Fotóssegéd asztali alkalmazás/Fotosseged/Fotosseged/FormGepek.Designer.cs
+++ Fotóssegéd asztali alkalmazás/Fotosseged/Fotosseged/FormGepek.Designer.cs
@@ -30,13 +30,33 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fényképezőgépekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.objektívekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.felhasználókToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.névjegyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.kilépésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.sToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -48,7 +68,7 @@
             this.objektívekToolStripMenuItem,
             this.felhasználókToolStripMenuItem,
             this.névjegyToolStripMenuItem,
-            this.kilépésToolStripMenuItem});
+            this.exitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(989, 24);
@@ -62,6 +82,12 @@
             this.fényképezőgépekToolStripMenuItem.Name = "fényképezőgépekToolStripMenuItem";
             this.fényképezőgépekToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
             this.fényképezőgépekToolStripMenuItem.Text = "Fényképezőgépek";
+            // 
+            // sToolStripMenuItem
+            // 
+            this.sToolStripMenuItem.Name = "sToolStripMenuItem";
+            this.sToolStripMenuItem.Size = new System.Drawing.Size(79, 22);
+            this.sToolStripMenuItem.Text = "s";
             // 
             // objektívekToolStripMenuItem
             // 
@@ -82,12 +108,13 @@
             this.névjegyToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
             this.névjegyToolStripMenuItem.Text = "Névjegy";
             // 
-            // kilépésToolStripMenuItem
+            // exitToolStripMenuItem
             // 
-            this.kilépésToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.kilépésToolStripMenuItem.Name = "kilépésToolStripMenuItem";
-            this.kilépésToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
-            this.kilépésToolStripMenuItem.Text = "Kilépés";
+            this.exitToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.exitToolStripMenuItem.Text = "Kilépés";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // dataGridView1
             // 
@@ -97,22 +124,197 @@
             this.dataGridView1.Size = new System.Drawing.Size(518, 520);
             this.dataGridView1.TabIndex = 1;
             // 
-            // sToolStripMenuItem
+            // buttonLoad
             // 
-            this.sToolStripMenuItem.Name = "sToolStripMenuItem";
-            this.sToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.sToolStripMenuItem.Text = "s";
+            this.buttonLoad.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonLoad.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonLoad.Location = new System.Drawing.Point(557, 41);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(184, 30);
+            this.buttonLoad.TabIndex = 2;
+            this.buttonLoad.Text = "Adatok betöltése";
+            this.buttonLoad.UseVisualStyleBackColor = false;
             // 
-            // button1
+            // buttonEdit
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(557, 41);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 30);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Adatok betöltése";
-            this.button1.UseVisualStyleBackColor = false;
+            this.buttonEdit.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonEdit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonEdit.Location = new System.Drawing.Point(557, 94);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(184, 30);
+            this.buttonEdit.TabIndex = 3;
+            this.buttonEdit.Text = "Adatok módosítása";
+            this.buttonEdit.UseVisualStyleBackColor = false;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonDelete.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonDelete.Location = new System.Drawing.Point(557, 152);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(184, 30);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "Kijelölt sor törlése";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonNew.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonNew.Location = new System.Drawing.Point(780, 41);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(184, 30);
+            this.buttonNew.TabIndex = 5;
+            this.buttonNew.Text = "Új Fényképezőgép";
+            this.buttonNew.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(861, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(103, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(861, 134);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(103, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(861, 175);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(103, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(861, 294);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(103, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(861, 253);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(103, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(861, 213);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(103, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(861, 335);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(103, 20);
+            this.textBox7.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(809, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Gyártó";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(804, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Sorozat";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(812, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Típus";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(818, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Pixel";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(802, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Szenzor";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(799, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Objektív";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(830, 338);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Ár";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonSave.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonSave.Location = new System.Drawing.Point(557, 329);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(184, 30);
+            this.buttonSave.TabIndex = 20;
+            this.buttonSave.Text = "Mentés";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonAdd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonAdd.Location = new System.Drawing.Point(780, 378);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(184, 30);
+            this.buttonAdd.TabIndex = 21;
+            this.buttonAdd.Text = "Adatok Felvitele";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonCancel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonCancel.Location = new System.Drawing.Point(557, 378);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(184, 30);
+            this.buttonCancel.TabIndex = 22;
+            this.buttonCancel.Text = "Mégse";
+            this.buttonCancel.UseVisualStyleBackColor = false;
             // 
             // FormGepek
             // 
@@ -120,7 +322,27 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(989, 573);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonNew);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonLoad);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
@@ -145,8 +367,28 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ToolStripMenuItem felhasználókToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem névjegyToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem kilépésToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sToolStripMenuItem;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonCancel;
     }
 }
--- Fotóssegéd asztali alkalmazás/Fotosseged/Fotosseged/FormGepek.cs
+++ Fotóssegéd asztali alkalmazás/Fotosseged/Fotosseged/FormGepek.cs
@@ -15,16 +15,60 @@ namespace Fotosseged
         public FormGepek()
         {
             InitializeComponent();
+
         }
 
         private void FormGepek_Load(object sender, EventArgs e)
         {
+            beallitVezerloketIndulaskor();
+        }
+
+        private void beallitVezerloketIndulaskor()
+        {
+            buttonAdd.Visible = false;
+            buttonCancel.Visible = false;
+            buttonDelete.Visible = false;
+            buttonEdit.Visible = false;
+            buttonNew.Visible = false;
+
+            buttonLoad.Visible = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
 
         }
+
+
+
+
+
+
+
+
+
 
         private void objektívekToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
+    
 }
