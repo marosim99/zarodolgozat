1bee6a557f9d8d1534d2fa5e8ac6b36c03e641ad
Márk Marosi <marosim99@gmail.com>
2019-04-01 11:29:01 +0200

    Offline elérési útvonalak, dokumentáció befejezve, PDF-be exportálva

--- Fotóssegéd weboldal/php/reg.php
+++ Fotóssegéd weboldal/php/reg.php
@@ -3,11 +3,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
     <script src="../JS/jquery-3.3.1.min.js" charset="UTF-8"></script>
+    <script src="../JS/bootstrap.min.js"></script>
     <script src="../JS/regValidate.js" charset="UTF-8"></script>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/bootstrap.min.css">
    <link rel="stylesheet" href="../style/styles.css">
 
     <title>Fotós segéd</title>
